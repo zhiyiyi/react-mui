f7ffd5dc018a60de405b6502164d952352f205e6
zhiyiyi <chenzhiyi401@gmail.com>
2023-10-15 21:17:47 -0700

    Change status for Square

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import { useState } from 'react';
 
-function Square({value}) {
+function Square() {
+  const [value, setValue] = useState(null)
+  
+  function handleClick() {
+    setValue('X')
+  }
+
   return (
-    <button className='square'>{value}</button>
+    <button className='square' onClick={handleClick}>{value}</button>
   )
 }
 
@@ -10,19 +16,19 @@ export default function Board() {
   return (
     <>
       <div className='board-row'>
-        <Square value='1'/>
-        <Square value='2'/>
-        <Square value='3'/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className='board-row'>
-        <Square value='4'/>
-        <Square value='5'/>
-        <Square value='6'/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className='board-row'>
-        <Square value='7'/>
-        <Square value='8'/>
-        <Square value='9'/>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
     )
