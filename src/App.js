cbceeb4cf1dd9b7a76a9e7d05e5a4ec3caaf0877
zhiyiyi <chenzhiyi401@gmail.com>
2023-10-18 23:26:43 -0700

    Record game history and add main Game component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import { useState } from 'react';
 
 function Square({value, onSquareClick}) {
-
   return (
     <button className='square' onClick={onSquareClick}>{value}</button>
   )
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquare] = useState(Array(9).fill(null));
+function Board({xIsNext, squares, onPlay}) {
 
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) return;
@@ -21,8 +18,7 @@ export default function Board() {
     } else {
       nextSquares[i] = 'O';
     }
-    setSquare(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -53,6 +49,30 @@ export default function Board() {
     </div>
     </>
     )
+}
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className='game-info'>
+        <ol>{}</ol>
+      </div>
+    </div>
+  )
+
+
 }
 
 function calculateWinner(squares) {
